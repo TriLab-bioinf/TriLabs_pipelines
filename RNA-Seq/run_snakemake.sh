9e033121731b51b1d460865a6cf004ec12867020
Hernan Lorenzi <Hernan.Lorenzi@nih.gov>
2026-01-06 15:37:02 -0500

    add parameter feature to support dry run

--- RNA-Seq/run_snakemake.sh
+++ RNA-Seq/run_snakemake.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
-#SBATCH --time=6:00:00 
+#SBATCH --time=10-00:00:00 
 
 SNAKEFILE=$1
+PARAMS=$2
 
 if [[ ! -e ${SNAKEFILE} ]]; then
-    echo; echo Please add Snakemake file name; echo
+    echo; echo Please enter path to snakemake file; echo 
     exit
 fi    
 
@@ -13,6 +14,5 @@ module load snakemake
 source ~/bin/myconda
 
 ### Run pipeline
-snakemake --profile ./config/snakemake_profile --snakefile ${SNAKEFILE} -p
+snakemake --profile ./config/snakemake_profile --snakefile ${SNAKEFILE} -p ${PARAMS}
  
-
